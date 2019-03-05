e0bc3ddce3b7945416835ed91bcc1ccb76b270cf
jellyannat <k.thompson22@hotmail.com>
2019-03-05 22:07:22 +0000

    Worked on histogram function, wrote code to retrive traces from files

--- histogram.py
+++ histogram.py
@@ -1,5 +1,6 @@
 import math
 from inspect import signature
+#import numpy as np
 
 
 class Descriptor():
@@ -34,44 +35,71 @@ def histogram(x):
     if descriptor.upper() <= descriptor.lower():
         raise Exception("Invalid bounds")
 
-    # Excludes element x(i) if the index is out of range            NEEDS WORK
-    # ncol_x = len(x)
-    # result = [None] * len(ncell)
-    # w = round((x-descriptor.lower()) / (descriptor.upper-descriptor.lower) * ncell + 0.25)
-    # for i in ncol_x:
-    #     index = w(i)
-    #     if (index >= 1) & (index <= ncell):
-    #         result(index) = result(index) + 1
+    ncol_x = len(x)
+    result = []        #vector to store the result
+    # Excludes element x(i) if the index is out of range (1 <= index <= ncell)
+    for i in range(ncol_x):
+        x0 = x[i]      #one particular point in window x
+        # Calculates the index of the histogram cells
+        bin_idx = round((x0 - descriptor.lower()) / (descriptor.upper - descriptor.lower) * ncell + 0.25)
+        if (bin_idx >= 1) & (bin_idx <= ncell):
+            result[bin_idx] = result[bin_idx] + 1
 
     # return descriptors & row vector containing the histogram
+    return result, descriptor
 
 
-def entropy(x, descriptor, approach, base):
+def entropy(x):
     """Function that estimates the entropy of a stationary signal with independent samples"""
 
-    sig = signature(entropy)
-    if len(sig.parameters) > 4:
-        raise Exception("Too many input arguments")
+    result, descriptor = histogram(x)
 
-    # sort out how many input arguments your function is going to take (will it take approach & base types?)
+#     # Tests to ensure the input arguments are right
+#     sig = signature(entropy)
+#     if len(sig.parameters) > 2:
+#         raise Exception("Too many input arguments")
+#
+#     if not isinstance(descriptor, Descriptor):
+#         raise TypeError("The descriptor is not of class Descriptor")
+#
+#     lower_bound = descriptor.lower()
+#     upper_bound = descriptor.upper()
+#     ncell = descriptor.ncell()
+#
+#     # initialising the return values
+#     estimate = 0
+#     sigma = 0
+#     count = 0
+#
+#     for j in range(ncell):
+#       if result(j)
+#
+#     # base transformation
+#     estimate = estimate / math.log(base)
+#     # n_bias = n_bias/math.log(base)         only if the approach is biased
+#     sigma = sigma / math.log(base)
+#
+#
+#############       MAIN        #############
+# Retrieves the traces from the files
+files = ["010403ba_0007_1.asc", "010403ba_0007_2.asc", "010403ba_0007_3.asc",
+         "010403ba_0007_4.asc", "010403ba_0007_5.asc", "010403ba_0007_6.asc"]
+trace = []
+sampling_rate = 256
+file_count = 0
+# Store the traces and time vectors in cell arrays
+for f in files:
+    file_count = file_count + 1
+    trace.append(open(f, "r"))        #want this to load the raw data
+    #t[file_count] = np.divide(range(1,(trace[file_count]))), sampling_rate)     #the time vector (in seconds)
 
-    if not isinstance(descriptor, Descriptor):
-        raise TypeError("The descriptor is not of class Descriptor")
+# for f in files:
+#     file_count = file_count + 1
+#     trace[file_count] = open(f, "r")        #want this to load the raw data
+#     #t[file_count] = numpy.divide(range(1,(trace[file_count]))), sampling_rate)     #the time vector (in seconds)
 
-    lower_bound = descriptor.lower()
-    upper_bound = descriptor.upper()
-    ncell = descriptor.ncell()
+# for count, f in enumerate(files):
+#     trace[count] = open(f, "r")
+#     t[count] = numpy.divide((1:len(trace[count])), sampling_rate)
 
-    # initialising the return values
-    estimate = 0
-    sigma = 0
-    count = 0
 
-    #add options for different approaches and biases
-
-    # calculate entropy equation
-
-    # base transformation
-    estimate = estimate / math.log(base)
-    # n_bias = n_bias/math.log(base)         only if the approach is biased
-    sigma = sigma / math.log(base)
